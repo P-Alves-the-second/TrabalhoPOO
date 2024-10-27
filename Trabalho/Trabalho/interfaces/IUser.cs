c03a19d6084b46eabe74be5b8fed0cb3e9e36b45
Pedro Alves <a27986@alunos.ipca.pt>
2024-10-27 09:02:57 +0100

    Tetse

---
+++ Trabalho/Trabalho/interfaces/IUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trabalho.models;
+
+namespace Trabalho.interfaces
+{
+    public interface IUser
+    {
+        string Name { get; set; }
+        string Email { get; set; }
+        string Password { get; set; }
+
+        User LogIn(User[] UserList,string email,string password);
+        User Register(string name,string email,string password);
+    }
+}
---
+++ Trabalho/Trabalho/models/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trabalho.interfaces;
+
+namespace Trabalho.models
+{
+    public abstract class User: IUser
+    {
+        private string name { get; set; }
+        private string email { get; set; }
+        private string password { get; set; }
+
+        public string Name  
+        {
+            get => name;
+            set => name = value;
+        }
+        public string Email
+        {
+            get => email;
+            set => email = value;
+        }
+        public string Password
+        {
+            get => password;
+            set => password = value;
+        }
+
+        public User LogIn(User[] UserList,string email,string password) 
+        {
+            foreach (User user in UserList) 
+            {
+                if (user.Email == email && user.Password == password) return user;
+            }
+            return null;
+        }
+
+        public virtual User Register(string name, string email, string password) { return null; }
+
+    }
+}
